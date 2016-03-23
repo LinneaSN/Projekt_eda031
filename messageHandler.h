b262e01573a4a0b691d70ae3343e7773e7fcbdca
Filip Johannesson <tfy12fjo@panter-12.student.lth.se>
2016-03-23 11:54:19 +0100

    first header file!

---
+++ messageHandler.h
@@ -0,0 +1,10 @@
+#ifndef MESSAGEHANDLER_H
+#define MESSAGEHANDLER_H
+class messageHandler{
+	public:
+		int serverRead(string s,vector<std::string> &cmds);
+		std::string serverWrite(int stuff,);
+		//En metod för varje commando
+	private:
+};
+#endif
