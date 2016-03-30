58aaf06b97246e983cbd701fc34accf761c964be
Filip Johannesson <tfy12fjo@mars-5.student.lth.se>
2016-03-30 11:14:00 +0200

    more messageHandler

--- messageHandler.h
+++ messageHandler.h
@@ -2,36 +2,40 @@
 #define MESSAGEHANDLER_H
 class messageHandler{
 	public:
+		messageHandler(connection c);
+
 		int serverRead(string s,vector<std::string> &cmds);
 		//std::string serverWrite(int stuff,);
 		//En metod för varje commando
-		std::string serverListNG(std::vector<std::string> NG);
-		std::string serverCreateNG(bool answer);
-		std::string serverDeleteNG(bool answer);
-		std::string serverListArt(std::vector<std::string> articles);
-		std::string serverCreateArt(bool answer);
-		std::string serverDeleteArt(int answer);
-		std::string serverGetArt(std::string &title, std::string &author, std::string &text);
+		void serverListNG(std::vector<std::string> NG);
+		void serverCreateNG(bool answer);
+		void serverDeleteNG(bool answer);
+		void serverListArt(std::vector<std::string> articles);
+		void serverCreateArt(bool answer);
+		void serverDeleteArt(int answer);
+		void serverGetArt(std::string &title, std::string &author, std::string &text);
 
 	//For client:
 		void clientRead(string answer);
-		std::string clientListNG();
-		std::string clientCreateNG(std::string NGname);
-		std::string clientDeleteNG(int newsgroup);
-		std::string clientListArt(int newsgroup);
-		std::string clientCreateArt(int newsgroup, std::string &title, std::string &author, std::string &text);
+		void clientListNG();
+		void clientCreateNG(std::string NGname);
+		void clientDeleteNG(int newsgroup);
+		void clientListArt(int newsgroup);
+		void clientCreateArt(int newsgroup, std::string &title, std::string &author, std::string &text);
 		void clientDeleteArt(int newsgroup, int article);
 		void clientGetArt(int newsgroup, int article);
 	private:
-        void sendByte(unsigned char code);
-        void sendCode(unsigned char code);
-        void sendInt(int value);
-        void sendIntParameter(int param);
-        void sendStringParameter(std::string param);
-        unsigned char recvByte();
-        unsigned char recvCode();
-        int recvInt();
-        int recvIntParameter();
-        std::string recvStringParameter();
+		connection conn;
+
+        	void sendByte(unsigned char code);
+        	void sendCode(unsigned char code);
+        	void sendInt(int value);
+        	void sendIntParameter(int param);
+        	void sendStringParameter(std::string param);
+        	unsigned char recvByte();
+        	unsigned char recvCode();
+        	int recvInt();
+        	int recvIntParameter();
+        	std::string recvStringParameter();
 };
 #endif
