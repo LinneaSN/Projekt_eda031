c37f9d9082f15c4406599b741ae616df815f5330
Filip Johannesson <tfy12fjo@mars-5.student.lth.se>
2016-03-30 09:24:30 +0200

    added methods to header file

--- messageHandler.h
+++ messageHandler.h
@@ -3,8 +3,25 @@
 class messageHandler{
 	public:
 		int serverRead(string s,vector<std::string> &cmds);
-		std::string serverWrite(int stuff,);
+		//std::string serverWrite(int stuff,);
 		//En metod för varje commando
+		std::string serverListNG(std::vector<std::string> NG);
+		std::string serverCreateNG(bool answer);
+		std::string serverDeleteNG(bool answer);
+		std::string serverListArt(std::vector<std::string> articles);
+		std::string serverCreateArt(bool answer);
+		std::string serverDeleteArt(int answer);
+		std::string serverGetArt(std::string &title, std::string &author, std::string &text);
+
+	//For client:
+		void clientRead(string answer);
+		std::string clientListNG();
+		std::string clientCreateNG(std::string NGname);
+		std::string clientDeleteNG(int newsgroup);
+		std::string clientListArt(int newsgroup);
+		std::string clientCreateArt(int newsgroup, std::string &title, std::string &author, std::string &text);
+		std::string clientDeleteArt(int newsgroup, int article);
+		std::string clientGetArt(int newsgroup, int article);
 	private:
 };
 #endif
