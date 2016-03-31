90980cb57e63a980fd38cd34d438bcbf7096a0de
Filip <filip.johannesson@gmail.com>
2016-03-31 10:55:06 +0200

    hard work or hardly working?

--- client.cc
+++ client.cc
@@ -3,15 +3,24 @@
 
 using namespace std;
 
-vector<int> NGdata(3,0);
+vector<int> NGdata(3,0); //List for newsgroup, article and 
 const set<string> correctCmds={"list","read","create","delete","exit"};
+map<string,int> currentNewsGroup;
 
 string parseCmd(string &input){
 	istringstream s(input);
 	string parse;
 	int numbargs;
 	while(s>>parse){
-		if(s=="list"){
+		if(parse=="list"){
+			if(input.size()<5){
+				clientListNG();
+				currentNewsGroup=clientRead();
+			} else {
+				s>>parse; //get string for newsgroup
+				clientListArt(currentNewsGroup[parse]);
+			}
+		}else if(parse=="read"){
 			
 		}
 	}
@@ -22,9 +31,12 @@ int main(argv, argc[]){
 	//istream i;
 	string input;
 	string sendstr;
+	istringstream i;
 	while(input!="exit"){
 		cin>>input;
-		if(correctCmds.find(input)!=correctCmds.end()){
+		istringstream i(input);
+		string test;
+		if(correctCmds.find(i>>test)!=correctCmds.end()){
 			sendstr=parseCmd(input);	
 		} else {
 			cout<<"Errornous input: "<<input<<endl;
