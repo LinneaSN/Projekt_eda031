ebbc681107ecc21cf6a61b4bbde854d68c0db346
Filip <filip.johannesson@gmail.com>
2016-03-31 08:51:00 +0200

    further client development

--- client.cc
+++ client.cc
@@ -6,8 +6,17 @@ using namespace std;
 vector<int> NGdata(3,0);
 const set<string> correctCmds={"list","read","create","delete","exit"};
 
-string parseCMD(string &input)
-
+string parseCmd(string &input){
+	istringstream s(input);
+	string parse;
+	int numbargs;
+	while(s>>parse){
+		if(s=="list"){
+			
+		}
+	}
+}
+	
 int main(argv, argc[]){
 	//Read and parse data
 	//istream i;
