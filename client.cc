cf297652b053ee98bee63afc64cc47fee1ac97a3
Filip <filip.johannesson@gmail.com>
2016-04-03 12:53:34 +0200

    working userCMD parser

--- client.cc
+++ client.cc
@@ -10,15 +10,19 @@ using namespace std;
 //vector<int> NGdata(3,0); //List for newsgroup, article and 
 const set<string> correctCmds({"list","read","create","delete","exit"});
 map<string,int> currentNewsGroup;//Stores names of newgroups and their numbers/indices
+string NG;
 
-string parseCmd(string &input){
+void printErrorMessage(){
+	cout<<"errornous command: no newsgroup selected! select a newgroup with 'list' command!"<<endl;
+}
+
+void parseCmd(string &input){
 	istringstream s(input);
 	string parse;
-	string NG;
 	int numbargs;
 	while(s>>parse){
 		if(parse=="list"){
-			if(input.length()<5){
+				if(input.length()<5){
 				cout<<"Listing NewsGroups.."<<endl;
 //				clientListNG();
 //				currentNewsGroup=clientRead();
@@ -28,24 +32,54 @@ string parseCmd(string &input){
 //				clientListArt(currentNewsGroup[NG]);
 			}
 		}else if(parse=="read"){
-			s>>parse;
-			int article=static_cast<int>(parse[0]);
-//			clientGetArt(currentNewsGroup[NG],article);	
+			if(!NG.empty()){
+				s>>parse;
+				cout<<"Reading article: "<<parse<<"in newsgroup:"<<NG<<endl;
+				int article=static_cast<int>(parse[0]);
+	//			clientGetArt(currentNewsGroup[NG],article);
+			}else{
+				printErrorMessage();
+			}
 		}else if(parse=="create"){
-			s>>parse>>NG;
-			if(parse=="Newsgroup"){
+			s>>parse;
+			if(parse=="Newsgroup"||parse=="newsgroup"){
+				s>>NG;
+				cout<<"Creating Newsgroup: "<<NG<<endl;
 //				clientCreateNG(NG);
 //				currentNewsGroups[NG];
-			} else if(parse=="Article"){
-				string title;
-				string author;
-				string text;
-				s>>title>>author>>text;
-//				clientCreateArt(currentNewsGroup[NG],title,author,text);
+			} else if(parse=="Article" || parse=="article"){
+				if(!NG.empty()){
+					string title;
+					string author;
+					string text;
+					s>>title>>author>>text;
+	//				clientCreateArt(currentNewsGroup[NG],title,author,text);
+					cout<<"Creating Article: Title: "<<title<<" author: "<<author<<" text: "<<text<<endl;	
+				} else {
+					printErrorMessage();
+				}
+			}
+		}else if(parse=="delete" ){
+			s>>parse;
+			if(parse=="Newsgroup" || parse=="newsgroup"){
+				s>>NG;
+				cout<<"deleting Newsgroup: "<<parse<<endl;
+				//clientDeleteNG(currentNewsGroup[NG]);
+			} else if(parse=="Article" || parse=="article"){
+				if(!NG.empty()){
+					s>>parse;
+					int article=static_cast<int>(parse[0]);
+					//clientDeleteArt(currentNewsGroup[NG],article);
+					cout<<"Deleting Article: "<<article<<" in newsgroup: "<<NG<<endl;
+				} else {
+					printErrorMessage();	
+				}
 			}
 		}
 	}
 }
+
+
 	
 int main(){
 	//messageHandler MH=new messageHandler(Connection)
@@ -59,9 +93,9 @@ int main(){
 		istringstream i(input);
 		string test;
 		i>>test;
-		cout<<input<<endl;
+	//	cout<<input<<endl;
 		if(correctCmds.find(test)!=correctCmds.end()){
-			sendstr=parseCmd(input);	
+			parseCmd(input);	
 		} else {
 			cout<<"Errornous input: "<<input<<endl;
 		}		
